43670165426d8ae69ffc6edcdbf9ccd562e059d9
mathrews <mateusps4gg@gmail.com>
2024-01-17 14:08:18 -0300

    refatorando o código

--- main.py
+++ main.py
@@ -18,28 +18,29 @@ def obter_links_videos(url):
 
     return links_videos
 
-def baixar_trecho_meio_video(url, destino):
+def baixar_trecho_do_meio(url, destino):
     yt = YouTube(url)
     
-    # Obtém a stream de maior resolução disponível
-    video_stream = yt.streams.get_highest_resolution()
+    # Calcula o tempo de início e fim para o trecho de 2 segundos a partir do meio
+    meio = yt.length / 2
+    inicio_trecho = max(meio - 1, 0)  # Começa 1 segundo antes do meio
+    fim_trecho = min(meio + 1, yt.length)  # Termina 1 segundo após o meio
 
-    if video_stream:
-        # Baixa o trecho do meio do vídeo
-        caminho_salvar = os.path.join(destino, f'{yt.title}_trecho_meio.mp4')
-        video_stream.download(output_path=destino, filename=f'{yt.title}_trecho_meio')
-        print(f'Trecho do meio do vídeo baixado: {caminho_salvar}')
-    else:
-        print(f'Não foi possível encontrar uma stream para o vídeo: {url}')
+    # Baixa apenas o trecho desejado
+    video_stream = yt.streams.filter(subtype='mp4', progressive=True).first()
+    caminho_trecho = os.path.join(destino, f'{yt.title}_trecho.mp4')
+    video_stream.download(output_path=destino, filename=f'{yt.title}_trecho', start=inicio_trecho, end=fim_trecho)
+
+    print(f'Trecho de 2 segundos do meio do vídeo baixado: {caminho_trecho}')
 
 if __name__ == "__main__":
     url_canal = 'https://www.youtube.com/@ujelbplus'  # Substitua pela URL do canal desejado
     links_videos = obter_links_videos(url_canal)
 
-    destino_videos = 'C:\\Teteus\\DownCutYT\\videos'
+    destino_videos = 'C:\\Downloads'
 
     if not os.path.exists(destino_videos):
         os.makedirs(destino_videos)
 
     for video_url in links_videos:
-        baixar_trecho_meio_video(video_url, destino_videos)
+        baixar_trecho_do_meio(video_url, destino_videos)
