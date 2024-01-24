bbb9505f5318ad75f345b1b42a7fd2895fa0ad59
mathrews <mateusps4gg@gmail.com>
2024-01-24 00:26:53 -0300

    version 0.2.0. toDo: cleancode (opcional) e transformar em um executavel para download

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from array import array
 import os
 from pytube import YouTube
 from googleapiclient.discovery import build
@@ -19,7 +20,7 @@ def obter_links_videos_api  (api_key, playlistId):
 
     return links_videos
 
-def baixar_trecho_do_meio(url, destino, index):
+def baixar_trecho_do_meio(url, destino, index, inicio=0, fim=0, confirmacao='n'):
    
     try:
         print(F'Baixa o vídeo url: {url}, destino: {destino} e index: {index}')
@@ -33,44 +34,69 @@ def baixar_trecho_do_meio(url, destino, index):
         video_stream.download(output_path=destino, filename=novo_caminho)
         caminho_original = os.path.join(destino, novo_caminho)
 
-       
-        # Calcula o tempo de início e fim para o trecho de x segundos a partir do meio
-        meio = yt.length / 2
-        inicio_trecho = meio - 2
-        fim_trecho = meio + 3
+        if confirmacao == 'n':
+            # Calcula o tempo de início e fim para o trecho de x segundos a partir do meio
+            meio = yt.length / 2
+            inicio_trecho = meio - inicio
+            fim_trecho = meio + fim
 
-        # Corta o trecho do vídeo
-        print(F'CORTANDO O VÍDEO')
-        caminho_trecho = os.path.join(destino, f'cute_{index}.mp4')
+            # Corta o trecho do vídeo
+            print(F'CORTANDO O VÍDEO')
+            caminho_trecho = os.path.join(destino, f'cute_{index}.mp4')
 
-        print(F'Caminho original {caminho_original}')
-        video_clip = VideoFileClip(caminho_original).set_duration(yt.length)
-        video_clip_subclip = video_clip.subclip(inicio_trecho, fim_trecho)
-        video_clip_subclip.write_videofile(caminho_trecho, codec='libx264')
-        print(f'Trecho de 2 segundos do meio do vídeo baixado: {caminho_trecho}')
-        
-        # Exclui o vídeo original
-        os.remove(caminho_original)
+            print(F'Caminho original {caminho_original}')
+            video_clip = VideoFileClip(caminho_original).set_duration(yt.length)
+            video_clip_subclip = video_clip.subclip(inicio_trecho, fim_trecho)
+            video_clip_subclip.write_videofile(caminho_trecho, codec='libx264')
+            print(f'Trecho de 2 segundos do meio do vídeo baixado: {caminho_trecho}')
+        if confirmacao == 's':
+            video_stream = yt.streams.filter(subtype='mp4', progressive=True, res='720p').first()
+            novo_caminho = f'novo_nome_{index}.mp4'
+            video_stream.download(output_path=destino, filename=novo_caminho)
+            caminho_original = os.path.join(destino, novo_caminho)
+            
     except Exception as e:   
         print(f"An exception occurred: {e}")
 
 
     print(f"________________________________________________________________________________")
 
-
-if __name__ == "__main__":
-    api_key = 'AIzaSyAux-INzqgqv1hEffmIx9s1lOdydq_tEwk'
-    channel_id = 'UCbqyhIVG2wd9DX6aoHW_6rw'
-    link_playlists = ['PLwdnFmzXKgfAzwpQKeSg6kd8FrOZ0Qkr4', 'PLwdnFmzXKgfDpr-pVfxOV_dNJuLzFn8RD', 'PLwdnFmzXKgfCa3fxrZ-KKadSrZv70wC2B']
-    links_videos= []
+def passar_playlist_ids (playlist=[]):
+    playlistIds: array = playlist
+    insercao = input("Digite o Id da playlist que você deseja baixar (se já houver adicionada todos os ids, digite 'prosseguir'): ")
+    if insercao == 'prosseguir' and playlistIds == []:
+        return 'sem playlists'
     
-    for link_playlist in link_playlists:
-        links_videos = links_videos + obter_links_videos_api(api_key, link_playlist)
+    if insercao == 'prosseguir':
+        return playlistIds
+    else: 
+        playlistIds.append(insercao)
+        print(playlistIds)
+        return passar_playlist_ids(playlistIds)
 
-    destino_videos = 'videos'
+def exec_all (): 
+    if __name__ == "__main__":
+        api_key = 'AIzaSyAux-INzqgqv1hEffmIx9s1lOdydq_tEwk'
+        channel_id = '' # id do canal
+        link_playlists = passar_playlist_ids() #id's das playlists
+        links_videos= []
+        
+        for link_playlist in link_playlists:
+            links_videos = links_videos + obter_links_videos_api(api_key, link_playlist)
 
-    if not os.path.exists(destino_videos):
-        os.makedirs(destino_videos)
+        destino_videos = 'videos'
 
-    for indice, video_url in enumerate(links_videos):
-        baixar_trecho_do_meio(video_url, destino_videos, indice)
+        if not os.path.exists(destino_videos):
+            os.makedirs(destino_videos)
+
+        confirmacao = input("Deseja baixar apenas os vídeos inteiros [s/n]: ")
+        if confirmacao == 'n':
+            inicio = int(input("Digite quantos segundos para antes do meio dos vídeo: "))
+            fim = int(input("Digite quantos segundos para depois do meio dos vídeo: "))
+            for indice, video_url in enumerate(links_videos):
+                baixar_trecho_do_meio(video_url, destino_videos, indice, inicio, fim, confirmacao)
+        if confirmacao == 's':
+            for indice, video_url in enumerate(links_videos):
+                baixar_trecho_do_meio(video_url, destino_videos, indice, inicio=0, fim=0, confirmacao=confirmacao) 
+
+exec_all()
